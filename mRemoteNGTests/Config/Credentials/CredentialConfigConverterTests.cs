7b8ce6750d78e6268a087fd8852b28ddfa64e8de
David Sparer <David.Sparer@pearson.com>
2016-07-25 11:25:57 -0600

    Began creating class for splitting old connections files to connection file + credential file

---
+++ mRemoteNGTests/Config/Credentials/CredentialConfigConverterTests.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using mRemoteNG.Config.Credentials;
+using NUnit.Framework;
+
+namespace mRemoteNGTests.Config.Credentials
+{
+    public class CredentialConfigConverterTests
+    {
+        private CredentialConfigConverter _credentialConfigConverter;
+
+        [SetUp]
+        public void Setup()
+        {
+            _credentialConfigConverter = new CredentialConfigConverter();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _credentialConfigConverter = null;
+        }
+
+        private string CreateTestConfConsFile()
+        {
+            var consFilePath = Path.GetTempFileName();
+            File.WriteAllText(consFilePath, Resources.testConfCons);
+            return consFilePath;
+        }
+
+        [Test]
+        public void EnsureCreatingATestConfConsFileWorks()
+        {
+            var confConsFilePath = CreateTestConfConsFile();
+            Assert.That(File.ReadAllText(confConsFilePath), Is.Not.Null);
+        }
+    }
+}
---
+++ mRemoteV1/Config/Credentials/CredentialConfigConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using mRemoteNG.Credential;
+
+
+namespace mRemoteNG.Config.Credentials
+{
+    public class CredentialConfigConverter
+    {
+        private IList<CredentialInfo> _credentialList;
+
+        public CredentialConfigConverter()
+        {
+            _credentialList = new List<CredentialInfo>();
+        }
+
+        public IList<CredentialInfo> BuildCredentialListFromConnectionFile()
+        {
+            GetUniqueCredentialsFromConnectionsFile();
+            return _credentialList;
+        }
+
+        private void GetUniqueCredentialsFromConnectionsFile()
+        {
+            
+        }
+    }
+}
